ae1f8fcdb506eec708d6c1a14fb92d7f71fb30dd
RichardClarosVelasquez <richard.claros101@gmail.com>
2023-09-23 22:55:59 -0400

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      © 2023 Planta Piloto de Reciclaje Residuos de Consatrucción y Demolición
+      Aranjuez, La Paz - Bolivia
+    </p>
+  </div>
+);
+
+export default Footer;
